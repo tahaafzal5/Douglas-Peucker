269c22f6c040ab05811925efa0b406d51c4bea3a
Taha Afzal <tahaafzal5@hotmail.com>
2024-01-21 01:57:02 -0600

    Draw a basic plot with annotations

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
+#include <cmath>
 #include <iostream>
 #include <matplot/matplot.h>
 
 int main()
 {
-  // Define data points
-  std::vector<double> x = {1, 2, 3, 4, 5};
-  std::vector<double> y = {2, 4, 1, 3, 5};
+  double epsilon = 0;
+
+  std::vector<double> xPoints;
+  std::vector<double> yPoints;
+
+  for (double x = 0.0; x <= 5.0; x += 0.1)
+  {
+    xPoints.push_back(x);
+    double y = std::exp(-x) * std::cos(2 * M_PI * x);
+    yPoints.push_back(y);
+  }
 
   // Create a plot
-  matplot::plot(x, y);
+  matplot::plot(xPoints, yPoints).get()->marker("o");
 
   // Show the plot
   matplot::show();
